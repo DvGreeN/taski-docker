6e640ae5da7aeb79130e78f0f65fb2d3199e3c02
DvGreeN <dvgreen@gmail.com>
2023-09-28 20:33:41 +0800

    Add workflow with PEP8 PsqSQL tests fix

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -4,17 +4,18 @@ from http import HTTPStatus
 from api import models
 from django.test import Client, TestCase
 
+
 class TaskiAPITestCase(TestCase):
     def setUp(self):
         self.guest_client = Client()
 
     def test_list_exists(self):
-        """Проверка доступности списка задач."""
+        """РџСЂРѕРІРµСЂРєР° РґРѕСЃС‚СѓРїРЅРѕСЃС‚Рё СЃРїРёСЃРєР° Р·Р°РґР°С‡."""
         response = self.guest_client.get('/api/tasks/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_task_creation(self):
-        """Проверка создания задачи."""
+        """РџСЂРѕРІРµСЂРєР° СЃРѕР·РґР°РЅРёСЏ Р·Р°РґР°С‡Рё."""
         data = {'title': 'Test', 'description': 'Test'}
         response = self.guest_client.post('/api/tasks/', data=data)
         self.assertEqual(response.status_code, HTTPStatus.CREATED)
